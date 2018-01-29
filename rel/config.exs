575d6082ce31c272b66bc50793a2e17935c855cb
Memory <memory@abhishekraval.com>
2018-01-29 04:53:10 +0000

    minor BugFix

--- rel/config.exs
+++ rel/config.exs
@@ -30,13 +30,13 @@ environment :dev do
   # dev mode.
   set dev_mode: true
   set include_erts: false
-  set cookie: :"7!ega!qL{42Cm:[JL4rwMfC{0TDHFo~?mwXE2D:>N~H5ee[PeOGRC`UlkN[|,__F"
+  set cookie: :"aksM;KnPnEE)coGTUr&FqlnP_IebrZ5)A8t;!nRpYvTj`j(QF%]dS%m84Z``L}$8"
 end
 
 environment :prod do
   set include_erts: true
   set include_src: false
-  set cookie: :"*qUG_NPfTQ6zm8NlVea2eM{N%5_UlCtE7F,u[EuH,cxB6HWFcI?nRMwOypM2Kr/O"
+  set cookie: :"0dhC3sKx4kg[WZjn}i,GbQ$yhreSBrGQ97kLK3EkQH_B~nO_4tv?erQ[gf),_GgC"
 end
 
 # You may define one or more releases in this file.
